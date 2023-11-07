6b12ef884b523f15375b2ccf4b36252081ae3014
Ion Stici <ionsticidev@gmail.com>
2023-11-07 10:06:38 +0200

    Update 'Travel List' Project

--- 05-travel-list/src/App.jsx
+++ 05-travel-list/src/App.jsx
@@ -1,16 +1,23 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-const initialItems = [
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: true },
-];
+// const initialItems = [
+//     { id: 1, description: "Passports", quantity: 2, packed: false },
+//     { id: 2, description: "Socks", quantity: 12, packed: true },
+// ];
 
 function App() {
+    const [items, setItems] = useState([]);
+
+    const addItem = (item) => {
+        setItems((prev) => [...prev, item]);
+    };
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <PackingStats />
+            <Form addItem={addItem} />
+            <PackingStats items={items} />
             <Stats />
         </div>
     );
@@ -20,38 +27,72 @@ function Logo() {
     return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ addItem }) {
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+
     function handleSubmit(event) {
         event.preventDefault();
+
+        if (!description) return;
+
+        const newItem = {
+            description,
+            quantity,
+            packed: false,
+            id: Date.now(),
+        };
+
+        // console.log(newItem);
+        addItem(newItem);
+
+        setDescription("");
+        setQuantity(1);
     }
 
     return (
         <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
-            <select>
+            <select
+                value={quantity}
+                onChange={({ target }) => setQuantity(+target.value)}
+            >
                 {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
                     <option key={num} value={num}>
                         {num}
                     </option>
                 ))}
             </select>
-            <input type="text" placeholder="Item..." />
+            <input
+                type="text"
+                placeholder="Item..."
+                value={description}
+                onChange={({ target }) => setDescription(target.value)}
+            />
             <button>Add</button>
         </form>
     );
 }
 
-function PackingStats() {
+Form.propTypes = {
+    addItem: PropTypes.func,
+};
+
+function PackingStats({ items }) {
     return (
         <div className="list">
             <ul>
-                {initialItems.map((item) => (
+                {items.map((item) => (
                     <Item key={item.id} item={item} />
                 ))}
             </ul>
         </div>
     );
 }
+
+PackingStats.propTypes = {
+    items: PropTypes.array,
+};
 
 function Item({ item }) {
     return (
