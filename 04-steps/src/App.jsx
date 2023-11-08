af1119e46302194dd2aa48009b5033e4e8767a26
Ion Stici <ionsticidev@gmail.com>
2023-11-08 09:00:30 +0200

    Improve the '04-steps' project by adding props.children feature

--- 04-steps/src/App.jsx
+++ 04-steps/src/App.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import "./index.css";
 import { useState } from "react";
 
@@ -7,7 +8,15 @@ const messages = [
     "Invest your new income 🤑",
 ];
 
-function App() {
+export default function App() {
+    return (
+        <div>
+            <Steps />
+        </div>
+    );
+}
+
+function Steps() {
     const [step, setStep] = useState(1);
     const [isOpen, setIsOpen] = useState(true);
 
@@ -36,29 +45,36 @@ function App() {
                         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
                     </div>
 
-                    <p className="message">
-                        Step {step}: {messages[step - 1]}
-                    </p>
+                    <StepMessage step={step}>
+                        {messages[step - 1]}
+                        <div className="buttons" style={{ marginTop: 8 }}>
+                            <Button
+                                bgColor="#e7e7e7"
+                                textColor="#333"
+                                onClick={() =>
+                                    alert(`Learn how to ${messages[step - 1]}`)
+                                }
+                            >
+                                Learn how
+                            </Button>
+                        </div>
+                    </StepMessage>
 
                     <div className="buttons">
-                        <button
-                            style={{
-                                backgroundColor: "#7950f2",
-                                color: "#fff",
-                            }}
+                        <Button
+                            textColor="#fff"
+                            bgColor="#7950f2"
                             onClick={handlePrevious}
                         >
-                            Previous
-                        </button>
-                        <button
-                            style={{
-                                backgroundColor: "#7950f2",
-                                color: "#fff",
-                            }}
+                            <span>👈</span> Previous
+                        </Button>
+                        <Button
+                            textColor="#fff"
+                            bgColor="#7950f2"
                             onClick={handleNext}
                         >
-                            Next
-                        </button>
+                            Next <span>👉</span>
+                        </Button>
                     </div>
                 </div>
             )}
@@ -66,4 +82,37 @@ function App() {
     );
 }
 
-export default App;
+function StepMessage({ step, children }) {
+    return (
+        <div className="message">
+            <h3>Step {step}</h3>
+            {children}
+        </div>
+    );
+}
+
+StepMessage.propTypes = {
+    step: PropTypes.number,
+    children: PropTypes.any,
+};
+
+function Button({ textColor, bgColor, onClick, children }) {
+    return (
+        <button
+            style={{
+                backgroundColor: bgColor,
+                color: textColor,
+            }}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+}
+
+Button.propTypes = {
+    textColor: PropTypes.string,
+    bgColor: PropTypes.string,
+    onClick: PropTypes.func,
+    children: PropTypes.any,
+};
