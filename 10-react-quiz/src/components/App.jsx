9c17e700de40476fb43c23d651340db30ce361f8
Ion Stici <ionsticidev@gmail.com>
2024-01-27 10:19:02 +0200

    Finish '10-react-quiz' project

--- 10-react-quiz/src/components/App.jsx
+++ 10-react-quiz/src/components/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useReducer } from "react";
+
 import Header from "./Header";
+import Footer from "./Footer";
 import MainLayout from "./MainLayout";
 import Loader from "./Loader";
 import Error from "./Error";
@@ -7,6 +9,10 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -14,6 +20,8 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -25,7 +33,11 @@ function reducer(state, action) {
       return { ...state, status: "error" };
 
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
 
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -42,16 +54,34 @@ function reducer(state, action) {
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
 
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
@@ -90,8 +120,25 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <Footer>
+              <Timer secondsRemaining={secondsRemaining} dispatch={dispatch} />
+
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
         )}
       </MainLayout>
     </div>
---
+++ 10-react-quiz/src/components/Timer.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+function Timer({ dispatch, secondsRemaining }) {
+  const mins = String(Math.floor(secondsRemaining / 60)).padStart(2, "0");
+  const secs = String(secondsRemaining % 60).padStart(2, "0");
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      dispatch({ type: "tick" });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [dispatch]);
+
+  return (
+    <div className="timer">
+      {mins}:{secs}
+    </div>
+  );
+}
+
+export default Timer;
