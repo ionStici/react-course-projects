317c063cbeb5065edf82a951a42ccbbe6936e119
Ion Stici <ionsticidev@gmail.com>
2024-02-02 11:28:04 +0200

    Update

--- 12-atomic-blog/src/App.js
+++ 12-atomic-blog/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { faker } from "@faker-js/faker";
 import Button from "./Button";
 import { PostProvider, usePosts } from "./PostContext";
+// import Test from "./Test";
 
 function createRandomPost() {
   return {
@@ -113,14 +114,17 @@ function List() {
   const { posts } = usePosts();
 
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      {/* <Test /> */}
+    </>
   );
 }
 
